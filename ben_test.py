4d606d09668dbaf6831d4ee97b0217fb95e8164d
Benoit-Mathey <146717796+Benoit-Mathey@users.noreply.github.com>
2024-04-23 17:27:53 +0200

    Create ben_test.py

---
+++ ben_test.py
@@ -0,0 +1,12 @@
+import json
+import numpy as np
+import pandas as pd
+
+# Load the JSON file into a DataFrame
+df = pd.read_json('Data/dataset.json')
+
+# Display the first few rows of the DataFrame
+print(df.head())
+
+m=2
+print(m)
