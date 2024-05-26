ed8856975d154f5381fef67138abaa931cdc03bc
martheri <marguerite.thery@epfl.ch>
2024-05-26 13:29:40 +0200

    bert fine-tuning

--- BERT/dataset/train_model.py
+++ BERT/dataset/train_model.py
@@ -13,59 +13,47 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import AutoModelForTokenClassification
 from transformers import TrainingArguments, Trainer
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 import torch 
 
 """Specify the number of epochs and batch size as command line arguments"""
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=5)
-parser.add_argument('--batch_size', type=int, default=16)
-parser.add_argument('--samples', type=int, default=200)
-args = parser.parse_args()
-nbr_french_epochs = args.french_epochs
-nbr_german_epochs = args.german_epochs
-batch_size = args.batch_size
-nbr_samples = args.samples 
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--epochs', type=int, default=1)
+# parser.add_argument('--batch_size', type=int, default=16)
+# parser.add_argument('--samples', type=int, default=200)
+# args = parser.parse_args()
+nbr_epochs = 1
+batch_size = 8
+nbr_samples = 100
 
-# Create training/testing sets
-IDS_TRAIN = '/work/FAC/HEC/DEEP/shoude/ml_green_building/Data/reference_ids_train.npy'
-IDS_TEST = '/work/FAC/HEC/DEEP/shoude/ml_green_building/Data/reference_ids_test.npy'
-
-ids_train = np.load(IDS_TRAIN, allow_pickle=True)
-ids_test = np.load(IDS_TEST, allow_pickle=True)
-
-"""French model"""
+"""BERT model"""
 
 # Read from files 
-PATH_FRENCH = '/work/FAC/HEC/DEEP/shoude/ml_green_building/Data/data_fr.pkl'
-PATH_FRENCH_SAVE = '/work/FAC/HEC/DEEP/shoude/ml_green_building/Data/Predictions/'
-
+#PATH_DATA = '/home/mthery/code/data/'
+PATH_DATA = 'BERT/dataset/'
 # Load data and create train/test set
-data_fr = pd.read_pickle(PATH_FRENCH)
-data_fr_train = data_fr[data_fr['Property Reference Id'].isin(ids_train)]
-data_fr_test = data_fr[data_fr['Property Reference Id'].isin(ids_test)]
-#data_fr_train, data_fr_test = train_test_split(data_fr, test_size=0.2, random_state=42)
+df_train = pd.read_csv(PATH_DATA + "train.csv")
+df_test = pd.read_csv(PATH_DATA + "test.csv")
 
-X = data_fr_train.rename(columns= {'Demand':'label'})
-# remove rows with missing labels or missing descriptions
-X = X[X['label'].notna()]
-X = X[X['tweet'].notna()]
+# Convert labels to float
+df_train['label'] = df_train['label'].astype(float)
+
+X = df_train
 dataset = Dataset.from_pandas(X[['tweet', 'label']], preserve_index=False) 
 dataset = dataset.train_test_split(test_size=0.1) 
-#dataset
 
-# This loads the model for the french language. 
 # Num_labels = 1 indicates we are doing a regression. 
 model = AutoModelForSequenceClassification.from_pretrained("google-bert/bert-base-multilingual-cased", num_labels=1)
 
 # Tokenize descriptions to transform text into 512 tokens.
-tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-multilingual-cased-base") 
+tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-multilingual-cased") 
 
-def tokenize_function_french(data):
+def tokenize_function(data):
     return tokenizer(text=data['tweet'], padding="max_length", truncation=True)
 
-tokenized_datasets = dataset.map(tokenize_function_french, batched=True)
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
 model.resize_token_embeddings(len(tokenizer))
 
@@ -81,7 +69,7 @@ training_args = TrainingArguments(output_dir="test_trainer",
                                   evaluation_strategy="epoch",
                                   per_device_train_batch_size=batch_size,
                                   per_device_eval_batch_size=8,
-                                  num_train_epochs=nbr_french_epochs,
+                                  num_train_epochs=nbr_epochs,
                                   save_total_limit = 2,
                                   save_strategy = "epoch",
                                   load_best_model_at_end="True"
@@ -109,13 +97,22 @@ trainer = Trainer(model=model)
 
 def pipeline_prediction_french(X_test):
   dataset = Dataset.from_pandas(X_test[['tweet']],preserve_index=True) 
-  tokenized_datasets = dataset.map(tokenize_function_french)
+  tokenized_datasets = dataset.map(tokenize_function)
   raw_pred, _, _ = trainer.predict(tokenized_datasets) 
   return raw_pred
 
-pred_french = pipeline_prediction_french(data_fr_test)
-pred_french= pd.DataFrame(pred_french).set_index(data_fr_test.index).sort_index()
-print("The poisson deviance for french data on valid set is:", mean_squared_error(data_fr_test['pred'], pred_french))
+# pred_french = pipeline_prediction_french(data_fr_test)
+# pred_french= pd.DataFrame(pred_french).set_index(data_fr_test.index).sort_index()
+# print("The accuracy for data on test set is:", mean_squared_error(data_fr_test['pred'], pred_french))
+
+used_data = df_test
+used_data['pred_bert'] = pipeline_prediction_french(df_test)
+acc = accuracy_score(used_data['label'], used_data['pred_bert'])
+F1 = f1_score(used_data['label'], used_data['pred_bert'], average='macro')
+Precision = precision_score(used_data['label'], used_data['pred_bert'], average='macro')
+Recall = recall_score(used_data['label'], used_data['pred_bert'], average='macro')
+
+print(f"Accuracy: {acc:.3f}, F1: {F1:.3f}, Precision: {Precision:.3f}, Recall: {Recall:.3f}")
 
 # Save french predictions
-pred_french.to_pickle(PATH_FRENCH_SAVE + 'pred_french.pkl')
+used_data.to_pickle(PATH_DATA + 'pred_bert.csv')
